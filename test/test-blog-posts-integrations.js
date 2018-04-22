44f6c49b91df322219f0d63fa481e45d514a97a2
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-04-22 11:13:22 -0500

    create test file and import modules

---
+++ test/test-blog-posts-integrations.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const expect = chai.expect;
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+function seedBlogPostData() {
+    console.info('seeding blog post data');
+    const seedData = [];
+
+    for (let i=1; i<=10; i++) {
+        seedData.push(generateBlogPostData());
+    }
+    return BlogPost.insertMany(seedData);
+}
+
+function generateBlogPostData() {
+    return {
+        author: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+        },
+        title: faker.name.title(),
+        content: faker.lorem.paragraph(),
+        created: faker.date.past()
+    };
+}
+
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
+
