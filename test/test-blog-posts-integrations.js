a8bd50cf9a2bcb0bf267452eaf03aef7dd27de7b
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-04-22 12:13:00 -0500

    add GET integration tests

--- test/test-blog-posts-integrations.js
+++ test/test-blog-posts-integrations.js
@@ -40,3 +40,82 @@ function tearDownDb() {
     return mongoose.connection.dropDatabase();
 }
 
+describe('Blog Post API resource', function() {
+
+    before(function() {
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    beforeEach(function() {
+        return seedBlogPostData();
+    });
+
+    afterEach(function() {
+        return tearDownDb();
+    });
+
+    after(function() {
+        return closeServer();
+    });
+
+    describe('GET endpoint', function() {
+
+        it("should return all existing blog posts", function() {
+            let res;
+            return chai.request(app)
+                .get('/posts')
+                .then(function(_res) {
+                    res = _res;
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.length.of.at.least(1);
+                    return BlogPost.count();
+                })
+                .then(function(count) {
+                    expect(res.body).to.have.lengthOf(count);
+                });
+        });
+
+        it("should return a single blog post if id is passed", function() {
+            let res;
+            return BlogPost
+                .findOne()
+                .then(function(post) {
+                    res = post;
+
+                    return chai.request(app)
+                        .get(`/posts/${post.id}`);
+                })
+                .then(function(_res) {
+                    expect(_res).to.have.status(200);
+                    expect(_res).to.be.an('object');
+                    expect(_res.body.id).to.equal(res.id);
+                });
+        });
+
+        it('should return posts with right fields', function() {
+            let resPost;
+            return chai.request(app)
+                .get('/posts')
+                .then(function(res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('array');
+                    expect(res.body).to.have.length.of.at.least(1);
+
+                    res.body.forEach(function(post) {
+                        expect(post).to.be.an('object');
+                        expect(post).to.include.keys('id', 'author', 'title', 'content', 'created');
+                    });
+                    resPost = res.body[0];
+                    return BlogPost.findById(resPost.id);
+                })
+                .then(function(post) {
+                    expect(resPost.id).to.equal(post.id);
+                    expect(resPost.author).to.equal(post.authorName);
+                    expect(resPost.title).to.equal(post.title);
+                    expect(resPost.content).to.equal(post.content);
+                    expect(resPost.created).to.equal(post.created.toISOString());
+                });
+        });
+    });
+});
