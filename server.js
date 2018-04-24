831e73d106839f6878c6ac470205a4aae4d8979a
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-04-23 21:30:33 -0500

    update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/posts', (req, res) => {
   BlogPost
     .find()
     .then(posts => {
-      res.json({posts: posts.map((post) => post.serialize())});
+      res.json(posts.map(post => post.serialize()));
     })
     .catch(err => {
       console.error(err);
